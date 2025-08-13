026d4a14256970f5402f87e0be678f389ffde6bb
Max Keneau <gart@gart.sh>
2025-08-13 07:37:23 -0400

    new images, convert everything to webp, hover to enlarge

---
+++ client/src/imageViewer.ts
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("img").forEach(img => {
+        img.addEventListener("mouseenter", () => {
+            const viewer = document.createElement("div");
+            viewer.classList.add("image-viewer");
+            viewer.innerHTML = `<img src="${img.src}" />`;
+            document.body.appendChild(viewer);
+
+            const bounds = img.getBoundingClientRect();
+
+            const mouseMoveListener = (event) => {
+                // if mouse moves out of image bounds, remove viewer
+                if (event.clientX < bounds.left || event.clientX > bounds.right || event.clientY < bounds.top || event.clientY > bounds.bottom) {
+                    const viewer = document.querySelector(".image-viewer");
+                    if (viewer) {
+                        document.removeEventListener("mousemove", mouseMoveListener);
+                        viewer.remove();
+
+                    }
+                }
+            }
+
+            document.addEventListener("mousemove", mouseMoveListener);
+
+        });
+
+
+
+
+    });
+})
